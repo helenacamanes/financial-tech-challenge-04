f7c3e91196092fcfceeb47bd0bb60bfa385027a6
helenacamanes <helenacamanes@gmail.com>
2026-05-11 18:03:20 -0300

    init: new architeture structure of react native financial app

--- financial/src/core/repositories/IFinanceRepository.ts
+++
@@ -1,6 +0,0 @@
-import { Transaction } from "../entities/transaction";
-
-export interface IFinanceRepository {
-  create(transaction: Transaction): Promise<void>;
-  getTransactions(userId: string): Promise<Transaction[]>;
-}
